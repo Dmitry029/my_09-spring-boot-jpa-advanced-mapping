2428af5ed328db729a527c3be22d7084bdb4728c
Dmitry029 <kdp.029029@cmail.com>
2025-06-08 18:47:54 +0300

    start project

---
+++ 04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -0,0 +1,82 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+
+    // annotate the fields with db column names
+
+    // ** set up mapping to InstructorDetail entity
+
+    // create constructors
+
+    // generate getter/setter methods
+
+    // generate toString() method
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @NonNull
+    @Column(name="first_name")
+    private String firstName;
+
+    @NonNull
+    @Column(name="last_name")
+    private String lastName;
+
+    @NonNull
+    @Column(name="email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor",
+            //fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                    CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Course> courses;
+
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
+                '}';
+    }
+
+    // add convenience methods for bi-directional relationship
+
+    public void add(Course tempCourse) {
+
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
+    }
+}
