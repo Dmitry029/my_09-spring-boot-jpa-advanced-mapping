4ce8c9bdf7e5d709b979dcfff181fc1facb00e64
Dmitry029 <kdp.029029@cmail.com>
2025-06-08 15:42:47 +0300

    add find courses by instructor id

--- 03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
+++ 03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -52,7 +52,7 @@ public class Instructor {
     private InstructorDetail instructorDetail;
 
     @OneToMany(mappedBy = "instructor",
-           // fetch = FetchType.EAGER,
+            //fetch = FetchType.EAGER,
             cascade = {CascadeType.PERSIST, CascadeType.MERGE,
                     CascadeType.DETACH, CascadeType.REFRESH})
     private List<Course> courses;
